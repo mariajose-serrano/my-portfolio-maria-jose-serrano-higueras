3a28b75e9d6048e665fddf79a2fdd9447cbe0b9e
Maria Jose Serrano Higueras <mariajosedoubletree@gmail.com>
2025-12-28 17:10:53 +0100

    trying to push to github

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://mariajose-serrano.github.io",
   base: "/my-portfolio-maria-jose-serrano-higueras/",
   build: { assets: "assets" },
 });
